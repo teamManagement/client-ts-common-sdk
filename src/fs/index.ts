c415039f432fec2245c64c37f0dc81c072905792
SuLinXin66 <15210204903@163.com>
2023-02-01 09:25:41 +0800

    增加读取文件的api

---
+++ src/fs/index.ts
@@ -0,0 +1,19 @@
+import { _teamworkSdk } from "../common";
+
+/**
+ * fs api
+ */
+export interface FsApi {
+  /**
+   * 读取文件到base64字符串
+   * @param localPath 本地文件的路径
+   */
+  readFileToBase64Str(localPath: string): Promise<string>;
+  /**
+   * 读取文件到text
+   * @param localPath 要读取的文件
+   */
+  readFileText(localPath: string): Promise<string>;
+}
+
+export const fs: FsApi = _teamworkSdk.fs;
